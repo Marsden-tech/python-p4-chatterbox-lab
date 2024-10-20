12429b8a721b463ddb0a35f59e0fc81e63fcacf8
Marsden <marsdenlwero@gmail.com>
2024-10-20 18:16:20 +0300

    completes lab

--- server/models.py
+++ server/models.py
@@ -12,3 +12,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f'<Message by {self.username}: {self.body[:10]}>'
